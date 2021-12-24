d336f706d419738af8572d2ced4154331c15da67
Vu Son Luong <sonluong999@gmail.com>
2021-12-24 11:52:22 -0500

    Preprocess data and update README.md

--- Data_collection.py
+++ Data_collection.py
@@ -19,10 +19,37 @@ print(selected_target) #Print the selected target
 activity = new_client.activity
 res = activity.filter(target_chembl_id = selected_target).filter(standard_type = "IC50")
 df = pd.DataFrame.from_dict(res)
-print(df.head(3)) #Print the first 3 row of the selected targets that have IC50 reported.
+print(df) #Print the selected targets that have IC50 reported.
 
 
 # Save the retrieved bioactivity data to a .csv file
 df.standard_type.unique()
 df.to_csv('bioactivity_data.csv', index = False) 
 
+# Handling missing data:
+
+df2 = df[df.standard_value.notna()]
+print(df2) #Print out data without including missing data
+
+# Data preprocessing:
+
+## Labelling compound to be active (IC50 <= 1000 nM), intermediate (1000 nM < IC50 < 10000 nM) and inactive (IC50 >= 10000):
+
+bioactivity_class = []
+for i in df2.standard_value:
+    if float(i) <= 1000.0:
+        bioactivity_class.append('active')
+    elif float(i) <= 10000.0:
+        bioactivity_class.append('intermediate')
+    else:
+        bioactivity_class.append('inactive')
+        
+## Create a dataframe contains molecule_chembl_id, canonical_smiles, standard_value and bioactivity_class:
+
+selection = ['molecule_chembl_id', 'canonical_smiles', 'standard_value']
+df3 = df2[selection]
+df3 = pd.concat([df3, pd.Series(bioactivity_class)], axis=1) # put the bioactivity class to the dataframe
+print(df3) #Print the created dataframe
+
+
+df3.to_csv('bioactivity_preprocessed_data.csv', index=False) #Save the created dataframe to a .csv file
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Drug_Discovery_Using_Machine_Learning
  
+- Using machine learning to help with drug discovery targeting Valosin-containing protein
+
+- Update log:
+-- 11:15 AM, Fri, Dec 24th, 2021: Retrieve bioactivity data
+-- 11:51 AM, Fri, Dec 24th, 2021: Preprocess data
+
  The code is inspired from <Python for Bioinformatics - Drug Discovery Using Machine Learning and Data Analysis>(https://www.youtube.com/watch?v=jBlTQjcKuaY)
